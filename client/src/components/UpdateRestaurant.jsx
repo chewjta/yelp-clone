bf71feae88385eb0d349181613491003912eb022
chewjta <chewjta@gmail.com>
2021-02-23 22:20:41 +0800

    first commit

---
+++ client/src/components/UpdateRestaurant.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { RestaurantsContext } from "../context/RestaurantsContext";
+import { useHistory } from "react-router-dom";
+
+const UpdateRestaurant = (props) => {
+  const { id } = useParams();
+  let history = useHistory();
+  const { restaurants } = useContext(RestaurantsContext);
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("Price Range");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await RestaurantFinder.get(`/${id}`);
+      setName(response.data.data.restaurant.name);
+      setLocation(response.data.data.restaurant.location);
+      setPriceRange(response.data.data.restaurant.price_range);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const updatedRestaurant = await RestaurantFinder.put(`/${id}`, {
+      name,
+      location,
+      price_range: priceRange,
+    });
+    history.push("/");
+  };
+
+  return (
+    <div>
+      <form action="">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            id="name"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="location">Location</label>
+          <input
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="price_range">Price Range</label>
+          <input
+            value={priceRange}
+            onChange={(e) => setPriceRange(e.target.value)}
+            id="price_range"
+            type="number"
+            className="form-control"
+          />
+        </div>
+        <button
+          className="btn btn-primary"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Update
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateRestaurant;
